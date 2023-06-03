9117b48cdd2495e3d0eb68cd8fc0220cb7931fe2
최재용 <133841448+choiyongyong0813@users.noreply.github.com>
2023-06-03 20:10:29 +0900

    Add files via upload

---
+++ JAVA_STUDY/Practice05.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Practice05 {
+
+	  public static void main(String args[]) {
+			int 	 x = 100;
+			double pi = 3.14;
+			char 	 ch = 'a';
+			String str = "abc";
+
+			System.out.println(x); 
+			System.out.println(pi); 
+			System.out.println(ch);
+			System.out.println(str);
+		}
+	}
